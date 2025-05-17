7553704402d5974112ab1ff496d1931990235673
hungln <ng.hung01.it@gmail.com>
2025-05-17 23:48:44 +0700

    init project

---
+++ src/app/(pages)/about/page.tsx
@@ -0,0 +1,7 @@
+// import cn from "@/lib/utils";
+
+export default function aboutPage() {
+  return (
+    <h1>ABOUT PAGE</h1>
+  )
+}
---
+++ src/app/(pages)/service/page.tsx
@@ -0,0 +1,7 @@
+// import cn from "@/lib/utils";
+
+export default function servicePage() {
+  return (
+    <h1>Service Page</h1>
+  )
+}
