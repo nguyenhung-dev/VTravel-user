21ec5ca49645183068d7cf743013e57bc85ed7fa
hungln <ng.hung01.it@gmail.com>
2025-05-21 13:57:37 +0700

    update

---
+++ src/app/(auth)/AuthDialog.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import LoginForm from "./login/loginForm";
+import RegisterForm from "./register/registerForm";
+
+type Props = {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export default function AuthDialog({ open, onOpenChange }: Props) {
+  const [isLogin, setIsLogin] = useState(true);
+
+  useEffect(() => {
+    if (open) {
+      setIsLogin(true)
+    }
+  }, [open])
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{isLogin ? "Đăng nhập" : "Đăng ký"}</DialogTitle>
+        </DialogHeader>
+
+        {isLogin ? (
+          <LoginForm onSwitch={() => setIsLogin(false)} />
+        ) : (
+          <RegisterForm onSwitch={() => setIsLogin(true)} />
+        )}
+      </DialogContent>
+    </Dialog>
+  )
+}
