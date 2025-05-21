30d0574eadd6b41a22f12205df59b7d38b5accc8
NguyenVanHuong <nguyenhuong29032005@gmail.com>
2025-05-22 01:17:25 +0700

    Update form UI

--- src/app/(auth)/login/loginForm.tsx
+++ src/app/(auth)/login/loginForm.tsx
@@ -8,13 +8,50 @@ type Props = {
 
 export default function LoginForm({ onSwitch }: Props) {
   return (
-    <form className="space-y-2">
-      <input type="email" placeholder="Email" className="w-full border p-2" />
-      <input type="password" placeholder="Mật khẩu" className="w-full border p-2" />
-      <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">Đăng nhập</button>
+      <form >
+        <div className={styles.logo}>
+          <img src="" alt="" />
+        </div>
+        <p className={styles.heading}>Đăng nhập hệ thống đặt vé du lịch bằng tài khoản VTravel của bạn</p>
+        <div>
+        <div className={styles.form_row}>
+          <label htmlFor="" className={styles.form_label}>Vui lòng nhập thông tin của bạn</label>
+          <input placeholder="Nhập Email hoặc SĐT tài khoản VTravel của bạn" name="email"  type="text" className={styles.form_input} />
+          <input placeholder="Nhập mật khẩu của bạn" name="pass"  type="text" className={styles.form_input} />
+        </div>
+        <div className={styles.form_btn_login}>
+          <button type="submit" className="">
+            <span> Đăng nhập</span>
+            </button>
+        </div>
+        </div>
+
+        <p className={styles.form_line}>Hoặc</p>
+
+        <div >
+          <div className={styles.form_btn_link}>
+            <button>
+              <img src="" alt="" />
+              <p>Đăng nhập với Google</p>
+            </button>
+          </div>
+          <div className={styles.form_btn_link}>
+            <button>
+              <img src="" alt="" />
+              <p>Đăng nhập với FaceBook</p>
+            </button>
+          </div>
+          <div className={styles.form_btn_link}>
+            <button>
+              <img src="" alt="" />
+              <p>Đăng nhập với Microsoft</p>
+            </button>
+          </div>
+        </div>
+
       <p className="text-sm text-center">
-        Bạn chưa có tài khoản?{" "}
-        <button type="button" onClick={onSwitch} className="text-blue-500 underline">Đăng ký</button>
+        Bạn chưa có tài khoản?{" "}s
+      <button type="button" onClick={onSwitch} className="text-blue-500 underline">Đăng ký</button>
       </p>
     </form>
   )
