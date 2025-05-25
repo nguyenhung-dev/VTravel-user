757d61c2a9946361ee87d45f8f9abfdbf1f3955a
hungln <ng.hung01.it@gmail.com>
2025-05-25 10:02:11 +0700

    update 25/5

--- src/app/layouts/home-section/banner/index.tsx
+++ src/app/layouts/home-section/banner/index.tsx
@@ -1,24 +1,37 @@
 'use client'
 
-import styles from './style.module.css'
-import { useTranslations } from 'next-intl'
+import styles from './style.module.css';
+import { useTranslations } from 'next-intl';
+import { motion } from 'framer-motion';
 
 export default function Banner() {
   const t = useTranslations()
 
   return (
-    <div className={styles.banner}>
-      <iframe
-        src="https://intro-vtravel.pages.dev/"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="autoplay"
-      ></iframe>
-      <div className={`${styles.overlay}`}></div>
-      <div className={`${styles.title}`}>
-        <h1>Khám phá Việt Nam</h1>
-      </div>
-    </div>
+    <>
+      <section className={styles.banner}>
+        <div className={styles.wrapper}>
+          <iframe
+            src="https://intro-vtravel.pages.dev/"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allow="autoplay"
+          ></iframe>
+          <div className={`${styles.overlay}`}></div>
+          <div className={`${styles.title}`}>
+            <motion.h1
+              className="text-white font-bold text-center"
+              initial={{ opacity: 0, scale: 0.3 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }}
+            >
+              Khám phá Việt Nam
+            </motion.h1>
+          </div>
+        </div>
+      </section>
+      <div className={`${styles.spacing}`}></div>
+    </>
   )
 }
