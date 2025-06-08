17ad3d1f7969bd89ddc642a9a041f7c33818ce79
hungln <ng.hung01.it@gmail.com>
2025-06-08 12:56:42 +0700

    update code 8/6

--- src/components/serviceCarousel/index.tsx
+++ src/components/serviceCarousel/index.tsx
@@ -1,65 +1,91 @@
-'use client';
+"use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
-import { FaStar } from 'react-icons/fa6';
+import Link from 'next/link';
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import 'keen-slider/keen-slider.min.css';
+import styles from './style.module.css';
+import "./btnanimation.css";
+import CustomButton from '@/components/customButton';
 
-interface IServiceItem {
-  id: number;
+interface IService {
+  id?: number;
   title?: string;
   description?: string;
   ImgUrl?: string;
-  price?: string;
-  buttonText: string;
+  price?: number;
+  buttonText?: string;
+  unit?: string;
   rating?: number;
 }
 
 type TProps = {
-  data: IServiceItem[];
+  services: IService[];
+  href?: string;
 }
 
-export default function ServiceCarousel({ data }: TProps) {
+export default function ServiceCarousel({ services, href = "/" }: TProps) {
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    slides: { perView: 1 },
+    breakpoints: {
+      "(min-width: 640px)": {
+        slides: { perView: 2, spacing: 15 },
+      },
+      "(min-width: 1024px)": {
+        slides: { perView: 4, spacing: 20 },
+      },
+    },
+  });
+
+  const prev = () => slider.current?.prev();
+  const next = () => slider.current?.next();
+
+  function formatCurrency(value?: number) {
+    return value?.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+  }
+
   return (
-    <div>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={{ clickable: true }}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        {data.map((item) => (
-          <SwiperSlide key={item.id}>
-            <div>
-              <div className='relative'>
-                <Image src={item.ImgUrl ?? ""} alt={item.title ?? ""} width={300} height={300} />
-                <div className='absolute top-[10px] left-[10px] flex items-center'>
-                  <span>{item.rating}</span>
-                  <FaStar className="text-yellow-400 text-[1rem]" />
+    <div className="relative">
+      <div ref={sliderRef} className="keen-slider">
+        {services.map((service) => (
+          <div key={service.id} className={`${styles.keenSliderSlid} keen-slider__slide`}>
+            <div className="relative rounded-[10px] h-full flex flex-col shadow-lg bg-[#ffffff80]">
+              <Image src={service.ImgUrl ?? "/images/avt-default.jpg"} alt={service.title ?? "dịch vụ"} width={300} height={200} className="object-cover w-full h-[250px] rounded-[10px]" />
+              <div className="p-4 flex flex-col justify-between gap-8 flex-1">
+                <div className='flex-1'>
+                  <h3>
+                    <Link href={href} className="font-bold text-[#2d2d2d] text-[24px] inline-block transition-all duration-300 hover:text-[#01b5f3] hover:underline">
+                      {service.title}
+                    </Link>
+                  </h3>
+                  <p>
+                    {service.description}
+                  </p>
+                  <div>
+                    <span>Chỉ từ</span>
+                    <span>{formatCurrency(service.price)}</span>
+                    <span>/{service.unit}</span>
+                  </div>
+                </div>
+                <div>
+                  <Link href={href ?? "/"} className="cssbuttons-io-button">{service.buttonText ?? "Xem chi tiết"}
+                    <div className="icon">
+                      <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+                    </div>
+                  </Link>
                 </div>
               </div>
-              <div>
-                <h4>{item.title}</h4>
-                <p>{item.price}</p>
-                <p>{item.description}</p>
-              </div>
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+          </div >
+        ))
+        }
+      </div >
+      <div className='absolute left-0 right-0 top-[50%] transform -translate-y-1/2 w-full flex justify-between z-10'>
+        <CustomButton onClick={prev} className={`${styles.btn} ${styles.prev}`}><IoIosArrowBack size={23} color='#fff' /></CustomButton>
+        <CustomButton onClick={next} className={`${styles.btn} ${styles.next}`}><IoIosArrowForward size={23} color='#fff' /></CustomButton>
+      </div>
+    </div >
   );
 }
--- src/layouts/home-section/service/index.tsx
+++ src/layouts/home-section/service/index.tsx
@@ -1,22 +1,21 @@
-import ServiceCarousel from "@/components/serviceCarousel";
+
 import SERVICEDATA from "@/data/service.json";
-import ButtonGlobal from "@/components/buttonGlobal";
+import ServiceCarousel from "@/components/serviceCarousel";
 
 export default function Service() {
   return (
     <section className="relative z-10 py-36 bg-[#ffffff]">
-      <div className="flex container m-auto" >
-        <div className="flex-1">
-          <ServiceCarousel data={SERVICEDATA} />
-        </div>
-        <div className="w-1/2">
+      <div className=" container m-auto" >
+        <div className="text-center mb-14">
           <p className="uppercase text-[#636363] font-bold text-[18px] mb-2 ">Dịch vụ nổi bật
           </p>
           <h3 className="uppercase text-[var(--color-primary)] text-4xl font-[900] leading-[1.3]">Khám phá dịch vụ tiện ích cùng VTravel</h3>
-          <p className="text-[18px] text-[var(--color-content)] mt-7">
-            VTravel mang đến cho bạn hệ sinh thái dịch vụ du lịch toàn diện, từ đặt xe, thuê phòng nghỉ, đến thuê hướng dẫn viên bản địa và tour trọn gói. Chúng tôi đồng hành cùng bạn trên mọi hành trình, giúp bạn tiết kiệm thời gian, chi phí và tận hưởng chuyến đi một cách trọn vẹn nhất. Với VTravel, mỗi chuyến đi là một trải nghiệm đáng nhớ.
+          <p className="text-[18px] text-[var(--color-content)] mt-3 w-[800px] m-auto">
+            VTravel – Hệ sinh thái du lịch toàn diện, giúp bạn đặt xe, phòng nghỉ, tour trọn gói và hướng dẫn viên dễ dàng. Tiết kiệm thời gian, chi phí và tận hưởng hành trình trọn vẹn!
           </p>
-          <ButtonGlobal text="Xem thêm" className="mt-10" />
+        </div>
+        <div>
+          <ServiceCarousel services={SERVICEDATA} />
         </div>
       </div >
     </section >
